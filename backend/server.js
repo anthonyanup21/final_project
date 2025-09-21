90f355ca77c3faf2fdc2137230da8520333dc6af
anthony anup <anup>
2025-09-21 16:06:30 +0530

    made changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
 import http from "http"
 import {Server} from "socket.io"
+import path from "path"
 
 dotenv.config()
 const app = express()
 const server=http.createServer(app)
-
+const __dirname=path.resolve()//backend folder location
 //socket.io
 export const io=new Server(server,{
     cors: {
@@ -40,7 +41,11 @@ io.on("connection",(socket)=>{
     })
 })
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+if(process.env.ENV!="production"){
+    app.use(cors({origin:"http://localhost:5173",credentials:true}))
+
+
+}
 app.use(express.json({ limit: "10mb" }))
 app.use(express.urlencoded({ extended: true }));
 
@@ -48,8 +53,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use("/api/auth",authRoutes)
 app.use("/api/message",messageRoutes)
+app.use(express.static(path.join(__dirname,"../frontend/dist")))//dist folder location
 
 
+
+//if we get aly request other than above url then send them the static file
+if(process.env.ENV=="production"){
+app.get(/.*/,(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})}
 
 connectDB().then(() => {
     server.listen(process.env.PORT, () => {
